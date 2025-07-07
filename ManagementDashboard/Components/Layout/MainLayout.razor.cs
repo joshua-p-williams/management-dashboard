bb6a34722a51b3df08b1f5597f37bb1c8eff8c27
Joshua Williams <jwilliams@sibanking.com>
2025-07-06 23:02:04 -0500

    Made the sidebar operate better on mobile devices

--- ManagementDashboard/Components/Layout/MainLayout.razor.cs
+++ ManagementDashboard/Components/Layout/MainLayout.razor.cs
@@ -1,13 +1,31 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.JSInterop;
 using ManagementDashboard.Core.Services;
 
 namespace ManagementDashboard.Components.Layout
 {
-    public partial class MainLayout
+    public partial class MainLayout : IDisposable
     {
         [Inject] public SettingsService SettingsService { get; set; } = default!;
         [Inject] public IJSRuntime JS { get; set; } = default!;
+        [Inject] public NavigationManager Navigation { get; set; } = default!;
+
+        private bool isSidebarOpen = false;
+
+        protected override void OnInitialized()
+        {
+            Navigation.LocationChanged += HandleLocationChanged;
+        }
+
+        private void HandleLocationChanged(object? sender, LocationChangedEventArgs e)
+        {
+            isSidebarOpen = false;
+            InvokeAsync(StateHasChanged);
+        }
+
+        private void ToggleSidebar() => isSidebarOpen = !isSidebarOpen;
+        private void CloseSidebar() => isSidebarOpen = false;
 
         protected override async Task OnInitializedAsync()
         {
@@ -18,6 +36,11 @@ namespace ManagementDashboard.Components.Layout
         {
             var theme = SettingsService.IsDarkMode ? "dark" : "light";
             await JS.InvokeVoidAsync("document.body.setAttribute", "data-bs-theme", theme);
+        }
+
+        public void Dispose()
+        {
+            Navigation.LocationChanged -= HandleLocationChanged;
         }
     }
 }
